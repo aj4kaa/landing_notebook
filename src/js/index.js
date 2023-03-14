3662dd39b007f8f81d15641c4d7f18462bdfba3c
Dmitry Pashkov <aimfury@gmail.com>
2023-03-14 21:30:12 +0300

    add slider, video section, form

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 "use sctrict"
 
 window.onload = () => {
-  console.log('1');
+	new Swiper('.image-slider', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			dynamicBullets: true
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+		mousewheel: {
+			sensitivity: 1
+		},
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: true
+		},
+		grabCursor: true,
+		loop: true,
+		autoHeight: true,
+	});
 }
